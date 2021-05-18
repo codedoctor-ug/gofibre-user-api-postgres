25e6660de6148919f5253ea944f52ce34db19063
Dr. Abiira Nathan <nabira2by2@gmail.com>
2021-05-18 05:33:07 +0300

    Add validation using https://github.com/go-playground/validator

--- models/models.go
+++ models/models.go
@@ -4,21 +4,46 @@ import (
 	"time"
 
 	"github.com/abiiranathan/gofibre-gorm-relations/db"
+	"github.com/go-playground/validator"
 	"github.com/gofiber/fiber/v2"
 )
 
+// `validate:"required,email,min=6,max=32"`
+
 type Profile struct {
 	ID        int       `json:"id" gorm:"not null;primaryKey"`
-	FullName  string    `json:"name" gorm:"size:50;not null"`
-	Mobile    string    `json:"mobile" gorm:"size:10;not null;unique"`
-	BirthDate time.Time `json:"birth_date" gorm:"not null"`
+	FullName  string    `json:"name" gorm:"size:50;not null" validate:"required"`
+	Mobile    string    `json:"mobile" gorm:"size:10;not null;unique" validate:"required,len=10"`
+	BirthDate time.Time `json:"birth_date" gorm:"not null" validate:"required"`
 }
 
 type User struct {
 	ID       int     `json:"id" gorm:"not null;primaryKey"`
-	Username string  `json:"username" gorm:"size:25; not null;unique;index"`
-	Email    string  `json:"email" gorm:"size:100; not null;unique;index"`
+	Username string  `json:"username" gorm:"size:25; not null;unique;index" validate:"required"`
+	Email    string  `json:"email" gorm:"size:100; not null;unique;index" validate:"required,email"`
 	Profile  Profile `json:"profile" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:ID"`
+}
+
+type ErrorResponse struct {
+	FailedField string
+	Tag         string
+	Value       string
+}
+
+func ValidateStruct(user User) []*ErrorResponse {
+	var errors []*ErrorResponse
+	validate := validator.New()
+	err := validate.Struct(user)
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			var element ErrorResponse
+			element.FailedField = err.StructNamespace()
+			element.Tag = err.Tag()
+			element.Value = err.Param()
+			errors = append(errors, &element)
+		}
+	}
+	return errors
 }
 
 func GetUsers(c *fiber.Ctx) error {
@@ -34,7 +59,15 @@ func CreateUser(c *fiber.Ctx) error {
 	user := new(User)
 
 	if err := c.BodyParser(user); err != nil {
-		c.Status(400).Send([]byte(err.Error()))
+		c.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	errors := ValidateStruct(*user)
+	if errors != nil {
+		c.JSON(errors)
+		return nil
 	}
 
 	db.Preload("Profile").Create(&user)
